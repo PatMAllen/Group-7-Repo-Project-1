5ef1c1dbd48db0c1851a924a0a011e2e84809e47
David Stumpe <dstumpe264@gmail.com>
2022-12-03 16:54:42 -0500

    pulled image from api, display img on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 // var marvel_hash = c782de09ab3d4d512283595db3e5905b;
 var nameEl = document.getElementById('character-name');
 var bioEl = document.getElementById('character-bio');
+var thumbNailEl = document.getElementById('char-thumbnail');
 
 function getAllMarvelCharactersByName(){
     var requestUrl = 'https://gateway.marvel.com:443/v1/public/characters?ts=1812&apikey=d0ef214546c2e9f0b1d4ba6d35921915&hash=c782de09ab3d4d512283595db3e5905b'
@@ -25,11 +26,14 @@ function getCharacterByName(){
             return response.json();
         })
         .then(function(data){
-            
+            var thumbnail = document.createElement('img')
+            thumbnail.src = data.data.results[0].thumbnail.path + '.' + data.data.results[0].thumbnail.extension;
             console.log(data);
             console.log(data.data.results[0].name);
+            console.log(data.data.results[0].thumbnail);
             nameEl.textContent = data.data.results[0].name;
             bioEl.textContent = data.data.results[0].description;
+            thumbNailEl.append(thumbnail);
         })
 };
 
